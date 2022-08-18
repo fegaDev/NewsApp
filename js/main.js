a4f51e721e316e4d6db950420dbbb5bf880fd8a6
fegaDev <fegadeveloper@gmail.com>
2022-08-18 18:15:48 -0500

    createNews

--- js/main.js
+++ js/main.js
@@ -4,9 +4,18 @@ let url = 'https://newsapi.org/v2/top-headlines?' +
 'sources=bbc-news&' +
 'apiKey=036d0e62c7724afea656f4f44b87b354';
 
-
+let imagen = document.getElementById("img");
+let autor = document.getElementById("titulo");
+let desc = document.getElementById("description");
+let button = document.getElementById("btn");
 fetch(url)
   .then((response) => response.json()) 
   .then((data) => {
-    console.log(data.articles); 
+    let img = data.articles[0].urlToImage;
+    imagen.src = img;
+    autor.innerHTML = data.articles[0].author;
+    desc.innerHTML = data.articles[0].description;
+    button.href = data.articles[0].url;
   });
+
+  console.log(button);
